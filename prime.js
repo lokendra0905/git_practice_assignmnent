4d959ffa53c66bd251cd4527904a59b5510b4bc6
lokendra0905 <bannalokendrapal2003@gmail.com>
2022-09-21 17:12:43 +0530

    code fix by Lokendra

--- prime.js
+++ prime.js
@@ -1,7 +1,14 @@
 // Check Whether num is prime number or not
 let num=67
+let Factor=0
 for(i=1;i<=num;i++){
-    if(num%1==0){
-        console.log(prime)
+    if(num%i==0){
+       Factor++
     }
+}
+
+if(Factor===2){
+    console.log(num,"is a Prime Number")
+} else {
+    console.log(num,"is not a Prime Number")
 }
